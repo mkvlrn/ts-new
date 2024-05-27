cf6360237f16e8e9e7bead4ee0a8c56827f3ce7e
Mike Valeriano <mkvlrn@gmail.com>
2024-05-27 00:36:09 -0300

    fix: vite project main import extension

--- templates/vite/src/index.tsx
+++ templates/vite/src/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from '#/app/main';
+import { App } from '#/app/main.jsx';
 import '#/index.css';
 
 const root = createRoot(document.querySelector('#root') as HTMLElement);
